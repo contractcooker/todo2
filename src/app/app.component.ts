b692b9e13e366ff5053d0d4b84e23c3bcd2108b4
contractcooker <58091974+contractcooker@users.noreply.github.com>
2020-04-30 15:30:57 -0400

    MVP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './interfaces/todo'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo2';
+  title = 'Tom\'s Todo List';
+  todos:Todo[] = [
+    {task:"test1", completed:false},
+  ]
+  newTask:string = "";
+  newCompleted:boolean = false;
+
+  addTask = function(ev:Event) {
+    this.todos.push({task:this.newTask, completed:this.newCompleted})
+  }
+
+  deleteTask(index) {
+    this.todos.splice(index, 1);
+  }
+
+  deleteSelectedTasks() {
+    for(var i=(this.todos.length -1); i > -1; i--) {
+      if(this.todos[i].completed) {
+        this.todos.splice(i, 1);
+      }
+    }
+  }
+
+  completeTask = function(ev:Event){
+    return true;
+  }
 }

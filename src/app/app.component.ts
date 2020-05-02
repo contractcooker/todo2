3848b26337c48d177e82768d3f5dfdd72fd91d8f
contractcooker <58091974+contractcooker@users.noreply.github.com>
2020-05-02 14:03:37 -0400

    Assessment completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,26 +11,61 @@ export class AppComponent {
   todos:Todo[] = [
     {task:"test1", completed:false},
   ]
+  filteredTodos:Todo[] = [];
   newTask:string = "";
   newCompleted:boolean = false;
 
-  addTask = function(ev:Event) {
-    this.todos.push({task:this.newTask, completed:this.newCompleted})
+  addTask(ev:Event) {
+    this.todos.push({task:this.newTask, completed:this.newCompleted});
+    this.filteredTodos=[];
+    this.newTask="";
   }
 
+  completeTask(){
+    return true;
+  }
+  
   deleteTask(index) {
+    
+    
     this.todos.splice(index, 1);
+    
+  }
+
+  editTask(index) {
+    
+    this.newTask=this.todos[index].task;
+    this.todos.splice(index, 1);
+    
+  }
+
+  editFilteredTask(index) {
+    
+    this.newTask=this.filteredTodos[index].task;
+    for(var i=(this.todos.length -1); i > -1; i--) {
+      if(this.todos[i].task.match(this.filteredTodos[index].task))
+      this.todos.splice(i, 1);
+    }
+    this.filteredTodos = [];
+    
   }
 
   deleteSelectedTasks() {
     for(var i=(this.todos.length -1); i > -1; i--) {
       if(this.todos[i].completed) {
         this.todos.splice(i, 1);
+        
       }
     }
   }
-
-  completeTask = function(ev:Event){
-    return true;
+  filterTasks(string){
+    
+    this.filteredTodos = []
+    for(var i=0; i < (this.todos.length); i++) {
+      if(this.todos[i].task.includes(string)) {
+        this.filteredTodos.push(this.todos[i])
+      }
+    }
   }
+  
 }
